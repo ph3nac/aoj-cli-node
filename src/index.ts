0c073ef034260073b280980f1d4e911ce95cbed6
ph3 <100564304+ph3nac@users.noreply.github.com>
2023-01-05 12:10:48 +0900

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,37 @@
 import figlet from "figlet";
-console.log(figlet.textSync("AOJ CLI TOOLS"));
+import { Command } from "commander";
+
+const program = new Command();
+
+program.name("aoj-cli-node").description("CLI to AOJ").version("0.0.1");
+
+program
+  .command("init")
+  .description("Initialize configuration")
+  .action(() => {
+    console.log(figlet.textSync("AOJ CLI TOOLS"));
+    console.log("init!");
+  });
+
+program
+  .command("gen <problem_id>")
+  .description("Generate a boilerplate code and test cases")
+  .action((problem_id) => {
+    console.log(problem_id);
+  });
+
+program
+  .command("test")
+  .description("Test the solution")
+  .action(() => {
+    console.log("test");
+  });
+
+program
+  .command("submit")
+  .description("Submit a source code")
+  .action(() => {
+    console.log("submit");
+  });
+
+program.parse();
